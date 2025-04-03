f19f75495d6e7d84554a4772fd1f54503f54e4f0
FELIPE PEDROSO CANTANHEDE <felipe.cantanhede@etec.sp.gov.br>
2025-04-03 15:37:28 -0300

    Lista de projetos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ListaTarefas from "./components/ListaTarefas";
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="tudo">
+      <h1 className="titulo">Gerenciador de Tarefas</h1>
+      <ListaTarefas />
+    </div>
+  );
+}
+
+export default App;
